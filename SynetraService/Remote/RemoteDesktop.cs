414550eaf072cb9db38f1ef51e1a9d6fb38d0d40
Elvis OHIN <elvisohin33@gmail.com>
2024-01-22 23:50:56 +0100

    Computers

---
+++ SynetraService/Remote/RemoteDesktop.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SynetraService.Remote
+{
+    public class RemoteDesktop
+    {
+        private TcpClient Client = new TcpClient();
+        private NetworkStream NetworkStream;
+        private int port;
+
+        private static Bitmap CaptureDesktopImage()
+        {
+            using var screenshot = new Bitmap(1920, 1080);
+            using (var g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(0, 0, 0, 0,
+                screenshot.Size, CopyPixelOperation.SourceCopy);
+            }
+            return screenshot;
+        }
+        public static async Task SendImageAsync()
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    // Remplacez cette URL par l'URL de votre service .NET
+                    string serviceUrl = "https://localhost:7082/ShareScreen/cartemere";
+                    byte[] imageBytes;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        CaptureDesktopImage().Save(ms, ImageFormat.Png);
+                        imageBytes = ms.ToArray();
+                    }
+                    var content = new ByteArrayContent(imageBytes);
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+
+                    var response = await httpClient.PostAsync(serviceUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("L'image a été envoyée avec succès.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Échec de l'envoi de l'image : " + response.ReasonPhrase);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors de l'envoi de l'image : " + ex.Message);
+            }
+        }
+    }
+}
--- SynetraService/Worker.cs
+++ SynetraService/Worker.cs
@@ -1,3 +1,7 @@
+using SynetraService.Models;
+using SynetraService.Remote;
+using System.Net.Http.Json;
+
 namespace SynetraService
 {
     public class Worker : BackgroundService
@@ -11,13 +15,28 @@ namespace SynetraService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            HttpClient client = new HttpClient();
+
+            var pc = new Computer
+            {
+                CarteMere = SystemInfo.GetMotherboardInfo(),
+                GPU = SystemInfo.GetGPUName(),
+                Os = SystemInfo.GetOperatingSystemInfo(),
+                Name = SystemInfo.GetComputerName(),
+                IDProduct = SystemInfo.GetWindowsProductId(),
+                Statut = true,
+                IsActive = true,
+                OperatingSystem = SystemInfo.GetSystemArchitecture()
+            };
+            await client.PostAsJsonAsync("https://localhost:7082/api/Computers", pc);
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    //await RemoteDesktop.SendImageAsync();
                 }
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
